e4d83fbf797da36f47443f4939368362923d6db2
karl.gong <karl.gong@ef.com>
2015-05-22 15:34:34 +0800

    Reorganize dir struct

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 here = path.abspath(path.dirname(__file__))
 # Get the long description from the relevant file
-with open(path.join(here, "README.rst"), encoding="utf-8") as f:
+with open(path.join(here, "README.rst")) as f:
     long_description = f.read()
 
 classifiers = ['License :: OSI Approved :: Apache Software License',
