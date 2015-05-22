c392e9efc74ce9b52c3bdb8ec639fae2a92e806a
karl.gong <karl.gong@ef.com>
2015-05-22 17:08:19 +0800

    Add description for options.

--- _ptest/config.py
+++ _ptest/config.py
@@ -45,33 +45,38 @@ class Config:
             self.__properties[property_match.group(1)] = property_match.group(2)
 
     def __parse_options(self, option_args):
-        parser = OptionParser(usage="", version="%prog 1.0")
+        parser = OptionParser(usage="ptest [options]", version="ptest 1.0.0",
+                              description="ptest is a light testing framework for Python.")
         parser.add_option("-w", "--workspace", action="store", dest="workspace", default=".", metavar="dir",
-                          help="Specify the workspace")
+                          help="Specify the workspace dir. Default value is current dir.")
         parser.add_option("-t", "--targets", action="store", dest="test_targets", default=None, metavar="targets",
-                          help="")
+                          help="Specify the test targets, separated by comma. Test target can be package/module/class/method.")
         parser.add_option("-i", "--includetags", action="store", dest="include_tags", default=None, metavar="tags",
-                          help="")
+                          help="Select test cases to run by tag.")
         parser.add_option("-e", "--excludetags", action="store", dest="exclude_tags", default=None, metavar="tags",
-                          help="")
+                          help="Select test cases not to run by tag. These tests are not run even if included with --includetags.")
         parser.add_option("-n", "--testexecutornumber", action="store", dest="test_executor_number", metavar="int",
-                          default=1, help="")
-        parser.add_option("-R", "--runfailed", action="store", dest="run_failed", default=None, metavar="file", help="")
+                          default=1, help="Specify the number of test executors. Default value is 1.")
+        parser.add_option("-R", "--runfailed", action="store", dest="run_failed", default=None, metavar="file",
+                          help="Specify the xunitxml path and run the failed test cases.")
         parser.add_option("-o", "--outputdir", action="store", dest="output_dir", default="test-output", metavar="dir",
-                          help="")
+                          help="Specify the output dir.")
         parser.add_option("-x", "--xunitxml", action="store", dest="xunit_xml", default="xunit-results.xml",
-                          metavar="file",
-                          help="")
+                          metavar="file", help="Specify the xunit_xm pathl")
         parser.add_option("-r", "--reportdir", action="store", dest="report_dir", default="html-report", metavar="dir",
-                          help="")
-        parser.add_option("-l", "--listener", action="store", dest="listener", default=None, metavar="file", help="")
-        parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, help="")
-        parser.add_option("-q", "--quiet", action="store_false", dest="verbose", default=False, help="")
+                          help="Specify the report dir.")
+        parser.add_option("-l", "--listener", action="store", dest="listener", default=None, metavar="file",
+                          help="Specify the path of test listener. The listener class must implement ptest.TestListener")
+        parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False,
+                          help="Set ptest console to verbose mode.")
         parser.add_option_group(
-            OptionGroup(parser, "User defined properties", "Define properties via -D<key>=<value>."))
+            OptionGroup(parser, "User defined properties",
+                        "Define properties via -D<key>=<value>. Get defined property via get_property() in ptest.config."))
         options, unknown_args = parser.parse_args(option_args)
         if options.test_targets and options.run_failed:
-            parser.error("options -t(--test_targets) and -R(--runfailed) are mutually exclusive.")
+            parser.error("Options -t(--test_targets) and -R(--runfailed) are mutually exclusive.")
+        if (options.test_targets is None) and (options.run_failed is None):
+            parser.error("You must specified one of the following options: -t(--test_target), -R(run_failed).")
         self.__options = options
 
 
