7e87168eef9490d8f167879539d59abfb3980c94
Karl Gong <karl.gong@outlook.com>
2015-09-16 11:01:43 +0800

    Format and small css change.

--- ptest/htmltemplate/report.js
+++ ptest/htmltemplate/report.js
@@ -1,4 +1,4 @@
-$(window).load(function(){
+$(window).load(function () {
     $(function () {
         $('.tree li:has(ul)').addClass('parent_li');
         $('.tree li.parent_li > span').on('click', function (e) {
@@ -15,12 +15,12 @@ $(window).load(function(){
     });
 });
 
-String.prototype.format = function(args) {
+String.prototype.format = function (args) {
     var result = this;
     if (arguments.length > 0) {
         if (arguments.length == 1 && typeof (args) == "object") {
             for (var key in args) {
-                if(args[key]!=undefined){
+                if (args[key] != undefined) {
                     var reg = new RegExp("({" + key + "})", "g");
                     result = result.replace(reg, args[key]);
                 }
@@ -29,26 +29,26 @@ String.prototype.format = function(args) {
         else {
             for (var i = 0; i < arguments.length; i++) {
                 if (arguments[i] != undefined) {
-                    var reg= new RegExp("({)" + i + "(})", "g");
+                    var reg = new RegExp("({)" + i + "(})", "g");
                     result = result.replace(reg, arguments[i]);
                 }
             }
         }
     }
     return result;
-}
+};
 
-initTree = function(testSuite) {
+initTree = function (testSuite) {
     var testSuiteNode = $('<li><span>{0}</span><ul></ul></li>'.format(testSuite.name));
     $('.tree').find(' > ul').append(testSuiteNode);
 
     var beforeSuite = testSuite.beforeSuite;
-    if (!beforeSuite.isEmpty){
+    if (!beforeSuite.isEmpty) {
         var beforeSuiteNode = $('<li><span>{0}</span></li>'.format(beforeSuite.fixtureType));
         testSuiteNode.find(' > ul').append(beforeSuiteNode);
     }
 
-    for (var i = 0; i < testSuite.testClasses.length; i ++) {
+    for (var i = 0; i < testSuite.testClasses.length; i++) {
         var testClass = testSuite.testClasses[i];
         var testClassNode = $('<li><span>{0}</span><ul></ul></li>'.format(testClass.name));
         testSuiteNode.find(' > ul').append(testClassNode);
@@ -59,7 +59,7 @@ initTree = function(testSuite) {
             testClassNode.find(' > ul').append(beforeClassNode);
         }
 
-        for (var j = 0; j < testClass.testGroups.length; j ++) {
+        for (var j = 0; j < testClass.testGroups.length; j++) {
             var testGroup = testClass.testGroups[j];
             var testGroupNode = $('<li><span>{0}</span><ul></ul></li>'.format(testGroup.name));
             testClassNode.find(' > ul').append(testGroupNode);
@@ -70,7 +70,7 @@ initTree = function(testSuite) {
                 testGroupNode.find(' > ul').append(beforeGroupNode);
             }
 
-            for (var k = 0; k < testGroup.testCases.length; k ++) {
+            for (var k = 0; k < testGroup.testCases.length; k++) {
                 var testCase = testGroup.testCases[k];
                 var testCaseNode = $('<li><span>{0}</span></li>'.format(testCase.name));
                 testGroupNode.find(' > ul').append(testCaseNode);
@@ -91,8 +91,8 @@ initTree = function(testSuite) {
     }
 
     var afterSuite = testSuite.afterSuite;
-    if (!afterSuite.isEmpty){
+    if (!afterSuite.isEmpty) {
         var afterSuiteNode = $('<li><span>{0}</span></li>'.format(afterSuite.fixtureType));
         testSuiteNode.find(' > ul').append(afterSuiteNode);
     }
-}
+};
